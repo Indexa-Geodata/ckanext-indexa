b8b269daf3f71968065875ab2803821d53bd3939
enprava <epradavazquez@gmail.com>
2023-03-20 16:29:44 +0100

    index.html practicamente hecho

--- ckanext/indexa/plugin.py
+++ ckanext/indexa/plugin.py
@@ -2,8 +2,18 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+def custom_tags():
+    return {'cienciaytecnologia': 'Ciencia y Tecnología', 'comercio': 'Comercio', 'culturayocio': 'Cultura y Ocio',
+            'demografia': 'Demografía', 'deportes': 'Deportes', 'economia': 'Economía',
+            'medioambiente': 'Medio ambiente', 'mediorural': 'Medio rural', 'salud': 'Salud',
+            'sectorpublico': 'Sector público', 'seguridad': 'Seguridad', 'sociedadybienestar': 'Sociedad y Bienestar',
+            'educacion': 'Educación', 'empleo': 'Empleo', 'energia': 'Energía', 'hacienda': 'Hacienda',
+            'industria': 'Industria', 'legislacionyjusticia': 'Legislación y Justicia', 'transporte': 'Transporte',
+            'turismo': 'Turismo', 'urbanismoeinfraestructura': 'Urbanismo e Infraestructura', 'vivienda': 'Vivienda'}
+
 class IndexaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,4 +21,7 @@ class IndexaPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
-            'indexa')
+                             'indexa')
+
+    def get_helpers(self):
+        return {'custom_tags': custom_tags}
